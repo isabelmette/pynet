49e69a2211ec8d976e79c6c08968b28775e6f8a2
kirstin <kirstin.heidler@student.hpi.uni-potsdam.de>
2013-01-05 22:08:53 +0100

    implemented some more tests

--- servent/Tasks.py
+++ servent/Tasks.py
@@ -1,6 +1,7 @@
+import sys
+import traceback
 
-
-
+noResult = object()
 
 class Task:
     def __init__(self, function, args=(), kw={}):
@@ -11,13 +12,52 @@ class Task:
         self.done = False
         self.succeeded = False
         self.failed = False
+        self.result = None
+        self.exception = None
+        self.exceptionType = None
+        self.traceback = None
+        self.generator = None
 
     def perform(self):
+        if self.done:
+            return noResult
         try:
-            self.function(*self.arguments, **self.keywords)
-        except Exception as err:
-            print(err)
+            rslt = self.function(*self.arguments, **self.keywords)
+            try:
+                yieldvalue = next(rslt)
+                if self.generator:
+                    yieldvalue = next(self.generator)
+                else:
+                    self.generator = rslt
+                self.result = yieldvalue
+            except StopIteration as stop:
+                self.exceptionType, self.exception, tb = sys.exc_info()
+                self.result = stop.value
+                self.done = True
+                self.succeeded = True
+            except TypeError:
+                self.result = rslt
+                self.done = True
+                self.succeeded = True
+                
+            try:
+                yieldvalue = next(self.function(*self.arguments, **self.keywords))
+                if self.generator:
+                        yieldvalue = next(self.generator)
+                else:
+                    self.generator = self.function(*self.arguments, **self.keywords)
+                self.result = yieldvalue
+                print("went to here")
+            except:
+                pass
+        except:
+            self.exceptionType, self.exception, tb = sys.exc_info()
+            #self.exception = err
+            self.traceback = tb.tb_next
+            #traceback.print_exception(self.exceptionType, self.exception, self.traceback)
             self.failed = True
-            return
-        self.done = True
-        self.succeeded = True
+            return noResult
+        
+        if self.result == None:
+            self.result = noResult
+        return self.result
